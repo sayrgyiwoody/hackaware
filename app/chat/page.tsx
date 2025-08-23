f0a6124c1bba49e62588282ae3ee2c084724518f
Woody <wytun8904@gmail.com>
2025-08-23 16:03:47 +0630

    bottom margin update

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -442,7 +442,7 @@ export default function ChatPage() {
             {!isUserAtBottom && 
             <button
               onClick={()=>scrollToBottom()}
-              className="fixed bottom-4 right-4 z-50 p-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-lg flex items-center justify-center animate-bounce"
+              className="fixed bottom-40  md:bottom-4 right-4 z-50 p-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-lg flex items-center justify-center animate-bounce"
             >
               <ChevronDown size={24} />
               </button>
